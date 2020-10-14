b34124f2d085bd2595b2ce3e00efcc48c1bc0990
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-10-14 14:24:50 +1100

    started api

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaskAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
